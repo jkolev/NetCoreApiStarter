d89f7c369a6632d210519db40e2bebf0319f09f5
jkolev <j.kolev@outlook.com>
2020-03-29 00:56:16 +0100

    Added RavenDb

--- Api/Startup.cs
+++ Api/Startup.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -10,6 +11,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Raven.Client.Documents;
+using Raven.Client.Documents.Session;
 
 namespace Api
 {
@@ -25,7 +28,31 @@ namespace Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var settings = new Settings();
+            Configuration.Bind(settings);
+
+            AddRavenDb(services, settings.Database);
+
             services.AddControllers();
+        }
+
+        private void AddRavenDb(IServiceCollection services, Settings.DatabaseSettings databaseSettings)
+        {
+            var store = new DocumentStore
+            {
+                Urls = databaseSettings.Urls,
+                Database = databaseSettings.DatabaseName,
+                Certificate = new X509Certificate2(databaseSettings.CertPath, databaseSettings.CertPass)
+            }.Initialize();
+
+            services.AddSingleton<IDocumentStore>(store);
+
+            services.AddScoped<IAsyncDocumentSession>(serviceProvider =>
+            {
+                return serviceProvider
+                    .GetService<IDocumentStore>()
+                    .OpenAsyncSession();
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
